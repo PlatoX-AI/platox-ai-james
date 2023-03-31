4af6a8bf8d49c4b40d939c4871faab3257a8eb0e
linanw <linanwang@msn.com>
2023-03-31 13:58:06 +0800

    Added docker-deploy

---
+++ docker-deploy/root/initialdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+james-cli adddomain mail.platox.ai
+
+james-cli adduser user01@mail.platox.ai 1234
+james-cli adduser user02@mail.platox.ai 1234
+james-cli adduser user03@mail.platox.ai 1234
