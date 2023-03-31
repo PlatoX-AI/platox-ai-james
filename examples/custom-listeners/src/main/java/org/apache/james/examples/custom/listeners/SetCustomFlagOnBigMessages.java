307b5f6ed5a5f63d0522520457ce06096f25182d
linanw <linanwang@msn.com>
2023-03-31 11:44:25 +0800

    Updated listener example with mail body access.

--- examples/custom-listeners/src/main/java/org/apache/james/examples/custom/listeners/SetCustomFlagOnBigMessages.java
+++ examples/custom-listeners/src/main/java/org/apache/james/examples/custom/listeners/SetCustomFlagOnBigMessages.java
@@ -19,9 +19,12 @@
 
 package org.apache.james.examples.custom.listeners;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import javax.inject.Inject;
 import javax.mail.Flags;
-
 import org.apache.james.events.Event;
 import org.apache.james.events.EventListener;
 import org.apache.james.events.Group;
@@ -33,14 +36,19 @@ import org.apache.james.mailbox.MessageUid;
 import org.apache.james.mailbox.events.MailboxEvents.Added;
 import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.model.MessageRange;
+import org.apache.james.mailbox.model.MessageResult;
+import org.apache.james.mailbox.model.FetchGroup;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * A Listener to determine the size of added messages.
  *
- * If the size is greater or equals than the BIG_MESSAGE size threshold ({@value ONE_MB}).
- * Then it will be considered as a big message and added BIG_MESSAGE {@value BIG_MESSAGE} flag
+ * If the size is greater or equals than the BIG_MESSAGE size threshold
+ * ({@value ONE_MB}).
+ * Then it will be considered as a big message and added BIG_MESSAGE
+ * {@value BIG_MESSAGE} flag
  *
  */
 class SetCustomFlagOnBigMessages implements EventListener.GroupEventListener {
@@ -67,28 +75,48 @@ class SetCustomFlagOnBigMessages implements EventListener.GroupEventListener {
         if (event instanceof Added) {
             Added addedEvent = (Added) event;
             addedEvent.getUids().stream()
-                .filter(messageUid -> isBig(addedEvent, messageUid))
-                .forEach(messageUid -> setBigMessageFlag(addedEvent, messageUid));
+                    .filter(messageUid -> isBig(addedEvent, messageUid))
+                    .forEach(messageUid -> setBigMessageFlag(addedEvent, messageUid));
         }
     }
 
     private boolean isBig(Added addedEvent, MessageUid messageUid) {
+        LOGGER.info("checking isBig...");
         return addedEvent.getMetaData(messageUid).getSize() >= ONE_MB;
     }
 
     private void setBigMessageFlag(Added addedEvent, MessageUid messageUid) {
         try {
+            LOGGER.info("Setting flag...");
             MailboxSession session = mailboxManager.createSystemSession(addedEvent.getUsername());
             MessageManager messageManager = mailboxManager.getMailbox(addedEvent.getMailboxId(), session);
 
+            // set the BIG_MESSAGE flag
             messageManager.setFlags(
-                new Flags(BIG_MESSAGE),
-                FlagsUpdateMode.ADD,
-                MessageRange.one(messageUid),
-                session);
-        } catch (MailboxException e) {
+                    new Flags(Flags.Flag.FLAGGED),
+                    FlagsUpdateMode.ADD,
+                    MessageRange.one(messageUid),
+                    session);
+            
+            // Retrieve the mail message
+            MessageResult messageResult = messageManager
+                    .getMessages(MessageRange.one(messageUid), FetchGroup.BODY_CONTENT, session).next();
+            
+            // below is code to save the message body to a file
+            // It was tested and proven working well
+            // I commented it out because it is may fail unit tests on dev machine
+            // to use it, uncomment the code below and comment out the line below 
+            // and build with unit tests, command: mvn clean install -DskipTests=true
+
+            // Path target = Path.of("/logs/emails/body.log");
+            // Files.createDirectories(target.getParent());
+            // Files.copy(messageResult.getBody().getInputStream(), target, StandardCopyOption.REPLACE_EXISTING);
+
+            LOGGER.info("Setting flag done.");
+        } catch (MailboxException | IOException e) {
             LOGGER.error("error happens when adding '{}' flag to the message with uid {} in mailbox {} of user {}",
-                BIG_MESSAGE, messageUid.asLong(), addedEvent.getMailboxId(), addedEvent.getUsername().asString(), e);
+                    BIG_MESSAGE, messageUid.asLong(), addedEvent.getMailboxId(), addedEvent.getUsername().asString(),
+                    e);
         }
     }
 
