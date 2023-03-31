6f07322a2ecdf70fd4ad02327065d98c681752dd
linanw <linanwang@msn.com>
2023-04-01 00:03:36 +0800

    Added smtpserver.xml and set the TLS cert

--- docker-deploy/docker-refresh.sh
+++ docker-deploy/docker-refresh.sh
@@ -1,12 +1,12 @@
 ## down
-sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml down
+# sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml down
 
 git -C $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../ pull origin master
-# cp  /usr/syno/etc/certificate/_archive/Urop5B/privkey.pem /volume1/docker/linanw/platox-ai-james/docker-deploy/root/conf/
-# cp  /usr/syno/etc/certificate/_archive/Urop5B/cert.pem /volume1/docker/linanw/platox-ai-james/docker-deploy/root/conf/
+sudo cp  /usr/syno/etc/certificate/_archive/Urop5B/privkey.pem /volume1/docker/linanw/platox-ai-james/docker-deploy/root/conf/
+sudo cp  /usr/syno/etc/certificate/_archive/Urop5B/cert.pem /volume1/docker/linanw/platox-ai-james/docker-deploy/root/conf/
 
 ## up
-sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml up -d
+# sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml up -d
 
 ## restart
-# sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml restart platox-ai-james
+sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml restart platox-ai-james
