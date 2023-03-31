b7fdcfb94aa55a74cff9a5a97597d2f09aacb6f8
linanw <linanwang@msn.com>
2023-03-31 17:17:44 +0800

    Fixed docker-refresh.sh

--- docker-deploy/docker-refresh.sh
+++ docker-deploy/docker-refresh.sh
@@ -1,3 +1,4 @@
-sudo docker-compose down
-git pull origin master
-sudo docker-compose up
+# sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml down
+git -C $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../ pull origin master
+# sudo /usr/local/bin/docker-compose -d -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml up
+sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml restart platox-ai-james
