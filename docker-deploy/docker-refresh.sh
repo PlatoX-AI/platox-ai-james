d34a983baba5ad24ef984c4124882bc606726ec5
linanw <linanwang@msn.com>
2023-03-31 22:07:14 +0800

    Fixed docker-refresh.sh

--- docker-deploy/docker-refresh.sh
+++ docker-deploy/docker-refresh.sh
@@ -6,7 +6,7 @@ git -C $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../ 
 # cp  /usr/syno/etc/certificate/_archive/Urop5B/cert.pem /volume1/docker/linanw/platox-ai-james/docker-deploy/root/conf/
 
 ## up
-sudo /usr/local/bin/docker-compose -d -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml up
+sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml up -d
 
 ## restart
 # sudo /usr/local/bin/docker-compose -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yml restart platox-ai-james
